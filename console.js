1accb79a17d63bbeccefb8e0da3d1531a8082093
Victor Dominguez <vdominguez1993@gmail.com>
2020-06-08 22:08:33 +0200

    Serialport and electron rebuild dependency added, added method to list ports on startup

--- console.js
+++ console.js
@@ -1,3 +1,16 @@
+var SerialPort = require("serialport");
+
+SerialPort.list().then(
+    ports => {
+        ports.forEach(port => {
+            console.log(`${port.comName}\t${port.pnpId || ''}\t${port.manufacturer || ''}`)
+        })
+    },
+    err => {
+        console.error('Error listing ports', err)
+    }
+);
+
 var cursor = {
     x: 0,
     y: 0,
