6b3df75f8cfcac37810f6cba1debc9e0e4604ace
Victor Dominguez <vdominguez1993@gmail.com>
2020-06-10 21:40:35 +0200

    Changed cursor philosophy

--- console.js
+++ console.js
@@ -24,61 +24,49 @@ var cursor = {
 };
 
 var text_area = document.getElementById("terminal");
+var pre_cursor = document.getElementById("terminal").childNodes[0];
 
 function print_to_console(data) {
-    console.log(data);
-    var text = new TextDecoder("utf-8").decode(data);
-    text_area.innerHTML += text;
-    moveCursor(text.length, 0);
+    // For each character
+    data.forEach(character => {
+        var char_string = String.fromCharCode(character);
+        // Add it before the cursor
+        pre_cursor.nodeValue += char_string;
+    });
+
     window.scrollTo(0, document.body.scrollHeight);
 }
 
 function keyInput(event) {
     var character;
     var code = (event.keyCode ? event.keyCode : event.which);
+    
     if (code == 13) { //Enter keycode
         character = "\r\n"
     } else {
         character = String.fromCharCode(event.keyCode);
     }
-    console.log(`Write ${character}`);
+
     port.write(character, function(err) {
         if (err) {
             return console.log('Error on write: ', err.message)
         }
-        console.log('message written')
     });
 }
 
 function keySpecial(event) {
     if (event.key == "Backspace") {
-        console.log("Delete");
-        moveCursor(-1, 0);
+        // Send the corresponding backspace value
     }
 }
 
-function moveCursor(x, y) {
-    // Make sure cursor does not go under 0
-    cursor.x = (cursor.x + x > 0) ? cursor.x + x : 0;
-    cursor.y = (cursor.y + y > 0) ? cursor.y + y : 0;
-
-    updateCursorPosition();
-}
-
-function updateCursorPosition() {
-    cursor.object.style.top = cursor.y + 'ch';
-    cursor.object.style.left = cursor.x + 'ch';
-}
-
-updateCursorPosition();
-
 window.onfocus = function() {
-    cursor.object.style.borderStyle = "none";
+    cursor.object.style.outlineStyle = "none";
     cursor.object.style.animation = "blink 1s linear infinite alternate";
 };
 
 window.onblur = function() {
     // Make cursor look like it's not active
     cursor.object.style.animation = "";
-    cursor.object.style.borderStyle = "solid";
+    cursor.object.style.outlineStyle = "solid";
 };
